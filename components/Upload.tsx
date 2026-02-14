48836011004b2cd3359d7c0c82202e489bac129d
Sabyrka <sabyrkazen@gmail.com>
2026-02-14 09:35:07 +0500

    feat: enhance Upload component with drag-and-drop file upload functionality

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,7 +1,41 @@
+import { useState } from 'react'
+import { useOutletContext } from 'react-router'
+import { UploadIcon } from 'lucide-react'
+
 export const Upload = () => {
+  const [file, setFile] = useState<File | null>(null)
+  const [isDragging, setIsDragging] = useState(false)
+  const [progress, setProgress] = useState(0)
+
+  const { isSignedIn } = useOutletContext<AuthContext>()
+
   return (
-    <div>
-      Upload
+    <div className="upload">
+      {!file ? (
+        <div className={`dropzone ${isDragging ? 'is-dragging' : ''}`}>
+          <input
+            type="file"
+            className="drop-input"
+            accept=".jpg,.jpeg,.png"
+            disabled={!isSignedIn}
+          />
+
+          <div className="drop-content">
+            <div className="drop-icon">
+              <UploadIcon size={20} />
+            </div>
+
+            <p>
+              {isSignedIn
+                ? 'Click to upload or just drag and drop'
+                : 'Sign in or sign up with Puter to upload'}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div>FILE</div>
+      )}
     </div>
+
   )
 }
