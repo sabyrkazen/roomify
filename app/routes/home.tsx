1f40ff2d6cee3f8949a9fbc3c1743ea048382ee9
Sabyrka <sabyrkazen@gmail.com>
2026-02-14 08:40:29 +0500

    feat: add useEffect to refresh authentication state on component mount

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from './+types/home'
 import { Navbar } from '../../components/Navbar'
+import { ArrowRight } from 'lucide-react'
+import { Button } from '../../components/ui/Button'
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +14,31 @@ export default function Home() {
   return (
     <div className="home">
       <Navbar />
-      <h1 className="text-3xl text-indigo-700 font-extrabold">Home</h1>
+
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+
+          <p>Introducing Roomify 2.0</p>
+        </div>
+
+        <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+
+        <p className="subtitle">
+          Roomify is an AI-first design environment that helps you visualize, render, and ship architectural projects
+          faster than ever.
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">Watch Demo</Button>
+        </div>
+      </section>
     </div>
   )
 }
